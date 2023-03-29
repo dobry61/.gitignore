5e9dd73cbf89b4c4fc06ec4954236bce944e377e
raisbuvaaa <niggard1998@mail.ru>
2023-03-29 13:15:41 +0500

    adding

--- data/config.py
+++ data/config.py
@@ -1,12 +1,16 @@
-from environs import Env
+# from environs import Env
+#
+# # environs kutubxonasidan foydalanish
+# env = Env()
+# env.read_env()
+#
+# # .env fayl ichidan quyidagilarni o'qiymiz
+# BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
+# ADMINS = env.list("ADMINS")  # adminlar ro'yxati
+# IP = env.str("ip")  # Xosting ip manzili
 
-# environs kutubxonasidan foydalanish
-env = Env()
-env.read_env()
-
-# .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
-ADMINS = env.list("ADMINS")  # adminlar ro'yxati
-IP = env.str("ip")  # Xosting ip manzili
-
+import os
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+ADMINS = os.environ.get("ADMINS")
+ip = os.environ.get('ip')
 kanallar = ['@Rasmlar_moshinalar','@dasturlash_boylab','@bellingam_saka_mount']
